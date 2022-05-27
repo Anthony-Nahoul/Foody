485d8f2e15e1a5222075af8c0208cc6b9af2fc64
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-05-27 23:01:31 +0300

    frontend homepage Adding content to main js

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,1 +1,15 @@
 //javascript
+document.addEventListener("DOMContentLoaded", function(){
+var nav_links = document.getElementById("nav_Links");
+document.getElementById("solid1").addEventListener("click",function() {hideMenu()});
+document.getElementById("solid2").addEventListener("click",function() {showMenu()});
+
+function showMenu(){
+    nav_links.style.right = "0";
+
+}
+function hideMenu(){
+    nav_links.style.right = "-200px";
+}
+
+})
