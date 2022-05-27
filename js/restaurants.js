7c4e0c53df40c2e51e08a37a81332ae37d4a2950
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-27 03:51:08 +0300

    Adding responsiveness and javascript to the page

--- js/restaurants.js
+++ js/restaurants.js
@@ -1,2 +1,8 @@
-//testing
-console.log("test successful")
+const transferlink = document.getElementById("title-card-1");
+const reviewbtn = document.getElementById("reviewbtn");
+transferlink.addEventListener("click", function(){
+    location.href = "file:///C:/Users/Anthony/Desktop/Foody/pages/restaurantdetails.html"
+})
+reviewbtn.addEventListener("click", function(){
+    location.href = "file:///C:/Users/Anthony/Desktop/Foody/pages/reviews.html"
+})
