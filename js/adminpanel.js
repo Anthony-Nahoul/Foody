5c4d15bdf15daede8101a36ec69f4d174a7aa063
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-28 09:47:18 +0300

    frontend-JS: Fixed the overlay bug

--- js/adminpanel.js
+++ js/adminpanel.js
@@ -1,28 +1,32 @@
 //JS for ADMIN PANEL
-let isClicked = false;
+
 let users = document.getElementById("idusers");
 let usersbtn = document.querySelector("#menu-users");
-if(isClicked == false){
-    usersbtn.addEventListener("click", usersClicked);
-    isClicked=true;
-}
-else{
-    usersbtn.removeEventListener("click", usersClicked);
-    // isClicked = false;
-}
-
-
+usersbtn.addEventListener("click", usersClicked);
+// this function will display the reviews table on menu users click and hide the other tables
 function usersClicked(){
+    reviews.style.display = "none";
+    restaurants.style.display = "none";
+    reviewsbtn.style.backgroundColor = "var(--bordeau)";
+    reviewsbtn.querySelector('a').style.color = "var(--white)";
+    restaurantsbtn.style.backgroundColor = "var(--bordeau)";
+    restaurantsbtn.querySelector('a').style.color = "var(--white)";
     users.style.display = 'block';
     usersbtn.style.backgroundColor = "var(--white)";
     usersbtn.querySelector('a').style.color = "var(--bordeau)";
 }
-// this function will display the reviews table on menu reviews click
+// this function will display the reviews table on menu reviews click and hide the other tables
 let reviewsbtn = document.querySelector('.reviewbtn');
 let reviews = document.querySelector('.list-reviews');
 reviewsbtn.addEventListener("click", reviewsClicked);
 function reviewsClicked(){
+    users.style.display = "none";
+    restaurants.style.display = "none";
     reviews.style.display = 'block';
+    usersbtn.style.backgroundColor = "var(--bordeau)";
+    usersbtn.querySelector('a').style.color = "var(--white)";
+    restaurantsbtn.style.backgroundColor = "var(--bordeau)";
+    restaurantsbtn.querySelector('a').style.color = "var(--white)";
     reviewsbtn.style.backgroundColor = "var(--white)";
     reviewsbtn.querySelector('a').style.color = "var(--bordeau)";
 }
@@ -30,9 +34,15 @@ function reviewsClicked(){
 let restaurantsbtn = document.querySelector('.restaurantsbtn');
 let restaurants = document.querySelector('.list-restaurants');
 restaurantsbtn.addEventListener("click", restaurantsClicked);
-
+// this function will display the restaurants table on menu reviews click and hide the other tables
 function restaurantsClicked(){
+    users.style.display = "none";
+    reviews.style.display = "none";
     restaurants.style.display = 'block';
+    usersbtn.style.backgroundColor = "var(--bordeau)";
+    usersbtn.querySelector('a').style.color = "var(--white)";
+    reviewsbtn.style.backgroundColor = "var(--bordeau)";
+    reviewsbtn.querySelector('a').style.color = "var(--white)";
     restaurantsbtn.style.backgroundColor = "var(--white)";
     restaurantsbtn.querySelector('a').style.color = "var(--bordeau)";
 }
