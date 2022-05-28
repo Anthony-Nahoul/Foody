7ec43e99c539fe42c5914090ad197b4191dab2fc
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-28 04:51:47 +0300

    Adding event listeners to manage the overlay

--- js/adminpanel.js
+++ js/adminpanel.js
@@ -1,31 +1,41 @@
 //JS for ADMIN PANEL
-// let toggle = document.getElementById('#toggle');
-// let navigation = document.getElementById('#navigation');
-// let main = document.getElementById('#main');
-
-// toggle.onclick = function(){
-//     navigation.classList.toggle('active');
-// }
-
-// this function will display the users table on menu users click
+let isClicked = false;
 let users = document.getElementById("idusers");
 let usersbtn = document.querySelector("#menu-users");
+if(isClicked == false){
+    usersbtn.addEventListener("click", usersClicked);
+    isClicked=true;
+}
+else{
+    usersbtn.removeEventListener("click", usersClicked);
+    // isClicked = false;
+}
 
-usersbtn.addEventListener("click", function(){
-    users.style.display = "block";
-})
+
+function usersClicked(){
+    users.style.display = 'block';
+    usersbtn.style.backgroundColor = "var(--white)";
+    usersbtn.querySelector('a').style.color = "var(--bordeau)";
+}
 // this function will display the reviews table on menu reviews click
 let reviewsbtn = document.querySelector('.reviewbtn');
 let reviews = document.querySelector('.list-reviews');
-reviewsbtn.addEventListener("click", function(){
+reviewsbtn.addEventListener("click", reviewsClicked);
+function reviewsClicked(){
     reviews.style.display = 'block';
-})
+    reviewsbtn.style.backgroundColor = "var(--white)";
+    reviewsbtn.querySelector('a').style.color = "var(--bordeau)";
+}
 
 let restaurantsbtn = document.querySelector('.restaurantsbtn');
 let restaurants = document.querySelector('.list-restaurants');
-restaurantsbtn.addEventListener("click", function(){
+restaurantsbtn.addEventListener("click", restaurantsClicked);
+
+function restaurantsClicked(){
     restaurants.style.display = 'block';
-})
+    restaurantsbtn.style.backgroundColor = "var(--white)";
+    restaurantsbtn.querySelector('a').style.color = "var(--bordeau)";
+}
 
 
 // if (users.style.display == "block"){
@@ -34,18 +44,18 @@ restaurantsbtn.addEventListener("click", function(){
 // }
 
 
-let menuitems = document.querySelectorAll(".navigation ul li");
-for(let item = 0; item < menuitems.length; item++){
-    menuitems[item].onclick = function(){
-        // defineActive(menuitems[item]);
-        // console.log(menuitems[item]);
-        // if (menuitems[item].classList.contains("active")){
+// let menuitems = document.querySelectorAll(".navigation ul li");
+// for(let item = 0; item < menuitems.length; item++){
+//     menuitems[item].onclick = function(){
+//         // defineActive(menuitems[item]);
+//         // console.log(menuitems[item]);
+//         // if (menuitems[item].classList.contains("active")){
             
-        // }
-        this.style.backgroundColor = "var(--white)";
-        this.querySelector('a').style.color = "var(--bordeau)";
-    }
-}
+//         // }
+//         this.style.backgroundColor = "var(--white)";
+//         this.querySelector('a').style.color = "var(--bordeau)";
+//     }
+// }
 // this function will define which button is active and deselect others
 function defineActive(target){
     menuitems.forEach((btn) => {
