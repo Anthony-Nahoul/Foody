150094b487ed414f8e6dbba4a60edecb0894a53e
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-28 23:07:39 +0300

    frontend: separate the mobile menu handling from mainpage files into common files for reusability

--- js/common.js
+++ js/common.js
@@ -5,3 +5,13 @@ function toggleDropdown(){
     let userdropdown=document.querySelector('.user-dropdown');
     userdropdown.classList.toggle('hide');
 }
+var nav_links = document.getElementById("nav_Links");
+document.getElementById("solid1").addEventListener("click",function() {hideMenu()});
+document.getElementById("solid2").addEventListener("click",function() {showMenu()});
+
+function showMenu(){
+    nav_links.style.right = "0";
+}
+function hideMenu(){
+    nav_links.style.right = "-200px";
+}
