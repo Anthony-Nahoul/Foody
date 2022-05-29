716e228ca2700d1fc095159510a5e26ee72907a0
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-05-29 03:09:45 +0300

    frontend-CSS Adding media query

---
+++ js/profile.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+var myInput = document.getElementById("password1");
+var myInput2 = document.getElementById("password2");
+
+var submit_button = document.getElementById("submit")
+
+submit_button.addEventListener("click", function () { return validate()})
+
+function validate() {
+    if (myInput.value != myInput2.value) {
+        alert("Passwords do not match.");
+        return false;
+    }
+    return true;
+    
+}
+
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+var passwords_match = document.getElementById("passwords_match");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+
+
+}
+
+})
