e5460d2a9bead97a46e88ad41d0c3dbe3bbc9b15
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-31 21:10:14 +0300

    frontend-JS: linking the API's to add a review and rate for a specific restaurant

--- js/restaurantdetails.js
+++ js/restaurantdetails.js
@@ -31,8 +31,10 @@ function starSet(index){
         //give all the stars up to and including the current clicked star the color gold
         stars[i].classList.add('given');
     }
-}
+    document.querySelector(".hidden-rate").value = index+1;
 
+}
+// POPUP MENU FOR REVIEWS
 let overlay=document.querySelector('.overlay');
 let close=document.querySelector('.overlay .close-icon');
 let reviewpopupbutton=document.querySelector('#review-popup-button')
@@ -42,3 +44,63 @@ reviewpopupbutton.addEventListener('click',toggleOverlay);
 function toggleOverlay(){
     overlay.classList.toggle('hide');
 }
+
+// LINKING API's
+
+// GET INFO FOR RESTAURANTS
+let restoid =1;
+ let userid = 3;   /*window.localStorage.getItem(id);*/
+let title = document.querySelector(".single-restaurant-title");
+let description = document.querySelector(".single-restaurant-description");
+let categoryname = document.querySelector(".single-restaurant-category");
+let address = document.querySelector(".single-restaurant-address");
+let data1 = new FormData();
+data1.append('id',restoid);
+axios({
+    method: 'POST',
+    url: 'http://localhost/foody-backend/getSpecificRestaurant.php',
+    data: data1,
+  }).then(function (response) {
+    console.log(response.data);
+    for (let i =0; i<(response.data).length;i++){
+        title.innerHTML = `${response.data[i]["name"]}`;
+        description.innerHTML = `${response.data[i]["description"]}`;
+        address.innerHTML = `${response.data[i]["address"]}`;
+        categoryname.innerHTML = `${response.data[i]["categoryname"]}`;
+    }
+  })
+
+  //ADD REVIEW 
+  let reviewbtn = document.querySelector(".submit-btn");
+  
+  reviewbtn.addEventListener("click",giveReview);
+  function giveReview(e){
+      e.preventDefault();
+      let reviewtext = document.querySelector(".review-text").value;
+      let starsrate = document.querySelector(".hidden-rate").value;
+      let data= new FormData();
+      
+      data.append('userid', userid);
+      data.append('restaurantid', restoid);
+      data.append('description', reviewtext);
+      axios({
+        method: 'POST',
+        url: 'http://localhost/foody-backend/addreview.php',
+        data: data,
+      }).then(function (response) { //ADD THE RATE 
+        console.log(response);
+        let data2 = new FormData();
+        data2.append('userid', userid);
+        data2.append('restaurantid', restoid);
+        data2.append('value', starsrate);
+        axios({
+            method: 'POST',
+            url: 'http://localhost/foody-backend/addrate.php',
+            data: data2,
+          }).then(function (response) {
+            console.log(response);
+          })
+      })
+  }
+
+
